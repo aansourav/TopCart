25f5d913b968ed29cb62ccf6f6c3764388842f88
SoUrAv <aansourav@gmail.com>
2024-06-06 07:58:52 +0600

    Final

---
+++ src/app/[locale]/privacy-policy/page.js
@@ -0,0 +1,46 @@
+const PrivacyPolicy = () => {
+    return (
+        <div className="bg-gray-100 p-8 mt-24 ">
+            <h1 className="text-xl font-bold text-center mb-4">
+                Privacy Policy
+            </h1>
+            <div className="max-w-4xl mx-auto text-gray-800 text-justify">
+                <h2 className="text-lg font-semibold">1. Introduction</h2>
+                <p className="mt-2">
+                    We value the privacy of our users and are committed to
+                    protecting your personal data. This policy outlines our
+                    practices concerning data collection, use, and sharing.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">
+                    2. Data Collection
+                </h2>
+                <p className="mt-2">
+                    We collect information that you provide directly to us when
+                    you use our services. This may include personal information
+                    such as your name, email address, and other contact details.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">3. Use of Data</h2>
+                <p className="mt-2">
+                    The data we collect is used to provide, maintain, and
+                    improve our services, as well as to develop new ones. We
+                    also use the information to communicate with you about our
+                    services, offers, promotions, and more.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">4. Data Sharing</h2>
+                <p className="mt-2">
+                    We do not share your personal data with third parties except
+                    as necessary to fulfill the purposes for which it was
+                    collected or as required by law.
+                </p>
+                <h2 className="text-lg font-semibold mt-4">5. Your Rights</h2>
+                <p className="mt-2">
+                    You have the right to access, correct, or delete your
+                    personal data. You can also object to the processing of your
+                    data in certain circumstances.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
